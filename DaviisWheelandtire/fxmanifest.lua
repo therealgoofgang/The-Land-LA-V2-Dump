789b502aa98be210549f69a53f8da1abd6c042e6
therealgoofgang <criatianguardado130@gmail.com>
2026-08-26 04:40:21 -0500

    Add files via upload

---
+++ DaviisWheelandtire/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Unorthodoxmodz'
+description 'DaviisRimnGo'
+version '3.0.0'
+
+this_is_a_map 'yes'
+
+escrow_ignore {
+
+}
+
+files {
+   "interiorproxies.meta",
+    'stream/*'
+}
+dependency '/assetpacks'
