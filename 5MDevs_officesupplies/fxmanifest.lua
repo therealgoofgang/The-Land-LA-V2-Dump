b20b0a61797bbb61e1c3cef7586499a70a96cd05
therealgoofgang <criatianguardado130@gmail.com>
2026-08-26 04:17:22 -0500

    Add files via upload

---
+++ 5MDevs_officesupplies/fxmanifest.lua
@@ -0,0 +1,9 @@
+
+fx_version 'cerulean'
+game { 'gta5' }
+name "5MDevs_officesupplies"
+author '5M Devs - https://www.5mdevs.store/shop'
+description 'Office Supplies Prop Pack by 5M Devs'
+lua54 'yes'
+data_file 'DLC_ITYP_REQUEST' 'stream/5mdevs_officesupplies.ytyp'
+dependency '/assetpacks'
